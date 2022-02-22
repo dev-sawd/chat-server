9b6fcb91d07dcc75e2d6b9fd1f2833e46d4a35bd
devsawd <devsawd@gmail.com>
2022-02-22 15:03:02 +0900

    port : 4000 기본적인 socket io 작성

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// path : chat/server/index.js
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const app = require('express')()
+const server = require('http').createServer(app)
+const cors = require('cors')
+const io = require('socket.io')(server,{
+  cors : {
+    origin :"*",
+    credentials :true
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+io.on('connection', socket=>{
+  socket.on('message',({name,message}) => {
+    io.emit('message',({name, message}))
+  })
+})
 
-module.exports = app;
+server.listen(4000, function(){
+  console.log('listening on port 4000');
+})
