45c902f38cdba035ada223ddab8f063c7b9732a7
요태 <devsawd@gmail.com>
2022-02-22 23:22:50 +0900

    userName, socketId를 관리하는 변수 추가 상대방에게 메세지를 보내는 기능 추가

--- app.js
+++ app.js
@@ -10,10 +10,34 @@ const io = require('socket.io')(server,{
   }
 });
 
+const userNameToSocketId = {}
+const socketIdToUserName = {}
+
 
 io.on('connection', socket=>{
-  socket.on('message',({name,message}) => {
-    io.emit('message',({name, message}))
+  console.log('소켓 연결됨')
+  socket.on('login',({userName}) => {
+    console.log('로그인', socket.id)
+    console.log('로그인', userName)
+    userNameToSocketId[userName] = socket.id
+    socketIdToUserName[socket.id] = userName
+    console.log(userNameToSocketId)
+    console.log(socketIdToUserName)
+  })
+  socket.on('sendMessage',({targetUserName, message}) => {
+    console.log('메세지 수신', targetUserName, message)
+    io.to(userNameToSocketId[targetUserName]).emit('message',({message}))
+  })
+  socket.on('disconnect', () => {
+    console.log('소켓 연결 종료', socket)
+    console.log('소켓 연결 종료')
+    var userName = socketIdToUserName[socket.id]
+
+    delete userNameToSocketId[userName]
+    delete socketIdToUserName[socket.id]
+
+    console.log(userNameToSocketId)
+    console.log(socketIdToUserName)
   })
 })
 
