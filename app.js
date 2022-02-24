867567d97f9bf4c30572c85efa072b17f979c64d
요태 <devsawd@gmail.com>
2022-02-24 21:34:41 +0900

    로그 삭제 로그인 했을때 returnLoginResponse에 기존에 로그인한 유저 네임을 리턴

--- app.js
+++ app.js
@@ -3,44 +3,42 @@
 const app = require('express')()
 const server = require('http').createServer(app)
 const cors = require('cors')
-const io = require('socket.io')(server,{
-  cors : {
-    origin :"*",
-    credentials :true
-  }
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        credentials: true
+    }
 });
 
 const userNameToSocketId = {}
 const socketIdToUserName = {}
 
 
-io.on('connection', socket=>{
-  console.log('소켓 연결됨')
-  socket.on('login',({userName}) => {
-    console.log('로그인', socket.id)
-    console.log('로그인', userName)
-    userNameToSocketId[userName] = socket.id
-    socketIdToUserName[socket.id] = userName
-    console.log(userNameToSocketId)
-    console.log(socketIdToUserName)
-  })
-  socket.on('sendMessage',({targetUserName, message}) => {
-    console.log('메세지 수신', targetUserName, message)
-    io.to(userNameToSocketId[targetUserName]).emit('message',({message}))
-  })
-  socket.on('disconnect', () => {
-    console.log('소켓 연결 종료', socket)
-    console.log('소켓 연결 종료')
-    var userName = socketIdToUserName[socket.id]
+io.on('connection', (socket) => {
+    socket.on('login', ({userName}) => {
+        userNameToSocketId[userName] = socket.id
+        socketIdToUserName[socket.id] = userName
+        console.log(userNameToSocketId)
+        console.log(socketIdToUserName)
+        socket.broadcast.emit('loginUser', userName)
+        socket.emit('returnLoginResponse', Object.keys(userNameToSocketId))
+    })
 
-    delete userNameToSocketId[userName]
-    delete socketIdToUserName[socket.id]
+    socket.on('sendMessage', ({targetUserName, message}) => {
+        io.to(userNameToSocketId[targetUserName]).emit('message', ({targetUserName, message}))
+    })
 
-    console.log(userNameToSocketId)
-    console.log(socketIdToUserName)
-  })
+    socket.on('disconnect', () => {
+        var userName = socketIdToUserName[socket.id]
+
+        delete userNameToSocketId[userName]
+        delete socketIdToUserName[socket.id]
+
+        console.log(userNameToSocketId)
+        console.log(socketIdToUserName)
+    })
 })
 
-server.listen(4000, function(){
-  console.log('listening on port 4000');
+server.listen(4000, function () {
+    console.log('listening on port 4000');
 })
